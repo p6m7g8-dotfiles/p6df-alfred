27fcb439467e4178f2ef65b3edf5711ead2863cc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-16 10:49:04 -0400

    Initialize Repository.

---
+++ init.zsh
@@ -0,0 +1,28 @@
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6df::modules::alfred::deps()
+#
+#>
+######################################################################
+p6df::modules::alfred::deps() {
+  ModuleDeps=(
+    p6m7g8-dotfiles/p6common
+    vitorgalvao/custom-alfred-iterm-scripts
+  )
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::alfred::external::brew()
+#
+#>
+######################################################################
+p6df::modules::alfred::external::brew() {
+
+  brew install --cask alfred
+
+  p6_return_Void
+}
